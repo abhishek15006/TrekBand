0b9df3a9b93675df09b0d620ba3b97d80754f433
Mohd Aadil <aadil15058@iiitd.ac.in>
2018-03-11 22:14:42 +0530

    Made some major changes for returning users (Somehow google sign in stopped working)

--- app/src/main/java/app/aadil/travelindia/SearchActivity.java
+++ app/src/main/java/app/aadil/travelindia/SearchActivity.java
@@ -5,8 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.*;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.*;
+//import com.google.android.gms.location.places.Place;
+//import com.google.android.gms.location.places.ui.*;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,21 +17,20 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i("PLACES", "Place: " + place.getName());
-            }
-
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i("ERROR", "An error occurred: " + status);
-            }
-        });
+//        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+//
+//        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//            @Override
+//            public void onPlaceSelected(Place place) {
+//                // TODO: Get info about the selected place.
+//                Log.i("PLACES", "Place: " + place.getName());
+//            }
+//
+//            @Override
+//            public void onError(Status status) {
+//                // TODO: Handle the error.
+//                Log.i("ERROR", "An error occurred: " + status);
+//            }
+//        });
     }
 }
