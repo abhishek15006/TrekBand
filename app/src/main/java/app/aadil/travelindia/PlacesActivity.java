8ad25b73231e9d681e698ff9ae2582b2baa385b5
Vaibhav <vaibhavkashyap2016@gmail.com>
2018-03-19 20:38:05 +0530

    Cardview in Search

---
+++ app/src/main/java/app/aadil/travelindia/PlacesActivity.java
@@ -0,0 +1,13 @@
+package app.aadil.travelindia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlacesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
